aa3c0b6ff3869cbb7223cfe1371750534c104c1f
Logan McAnsh <logan@mcan.sh>
2022-05-20 19:33:05 -0400

    chore: update example now that it works
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/server-runtime";
+import type { LoaderFunction, MetaFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
 import {
   Links,
   LiveReload,
@@ -6,6 +7,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 export const meta: MetaFunction = () => ({
@@ -14,15 +16,23 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const loader: LoaderFunction = () => {
+  return json({
+    date: new Date().toLocaleString(),
+  });
+};
+
 export default function App() {
+  let data = useLoaderData();
   return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
         <Outlet />
+        <footer style={{ marginTop: 16 }}>Page built at {data.date}</footer>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,39 @@
 import type { LoaderFunction, ActionFunction } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
 import { Form, useLoaderData } from "@remix-run/react";
+import { createCookie } from "@remix-run/netlify-edge";
 
-let projectName: string | null = null;
+let cookie = createCookie("projectName", {
+  httpOnly: true,
+  expires: new Date(Date.now() + 60_000),
+});
 
-export const loader: LoaderFunction = async () => {
-  return { name: projectName ?? "Create a Project Name" };
+export const loader: LoaderFunction = async ({ request }) => {
+  let projectName = await cookie.parse(request.headers.get("Cookie"));
+
+  return {
+    name: projectName ?? "Create a Project Name",
+    notes: ["remix is great", "so is deno"],
+  };
 };
 
 export const action: ActionFunction = async ({ request }) => {
   const body = await request.formData();
+  let projectName = body.get("projectName") as string;
 
-  console.log("body", Object.fromEntries(body));
-  projectName = body.get("projectName") as string;
-  console.log("projectName pulled from form data", projectName);
-  console.log(`request body is used ${request.bodyUsed}`);
-
-  console.log("action ran");
-
-  return new Response("");
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await cookie.serialize(projectName),
+    },
+  });
 };
 
 export default function Index() {
-  let project = useLoaderData();
+  let data = useLoaderData();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix {project.name}</h1>
+    <div>
+      <h1>Welcome to Remix {data.name}</h1>
       <Form reloadDocument method="post">
         <label>
           Project Name
@@ -33,6 +41,10 @@ export default function Index() {
         </label>
         <button type="submit">Submit</button>
       </Form>
+
+      {data.notes.map((note: string) => (
+        <p key={note}>{note}</p>
+      ))}
     </div>
   );
 }
